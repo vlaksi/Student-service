4843848c0e98e207aefef932395cf6b9f1334e8c
vlaksi <daksrapsody@gmail.com>
2019-12-28 04:18:51 +0100

    Prototip klase Predmet i init Profesora

--- StudentskaSluzba/src/model/Predmet.java
+++ StudentskaSluzba/src/model/Predmet.java
@@ -4,79 +4,76 @@ import java.util.List;
 
 /**
  * Model Predmeta
+ * 
  * @author Vaxi RA186/2017
  *
  */
 public class Predmet {
-	
+
 	String sifraPredmeta;
 	String nazivPredmeta;
 	Integer semestar;
 	Integer godinaStudija;
-	//TODO Profesor predmetniProfesor;
-	List<Student> listaStudenata;	// spisak studenata koji slusaju predmet
-	
-	
+	Profesor predmetniProfesor;
+	List<Student> listaStudenata; // spisak studenata koji slusaju predmet
+
 	public Predmet(String sifraPredmeta, String nazivPredmeta, Integer semestar, Integer godinaStudija,
-			List<Student> listaStudenata) {
+			Profesor predmetniProfesor, List<Student> listaStudenata) {
 		super();
 		this.sifraPredmeta = sifraPredmeta;
 		this.nazivPredmeta = nazivPredmeta;
 		this.semestar = semestar;
 		this.godinaStudija = godinaStudija;
+		this.predmetniProfesor = predmetniProfesor;
 		this.listaStudenata = listaStudenata;
 	}
-
 
 	public String getSifraPredmeta() {
 		return sifraPredmeta;
 	}
 
-
 	public void setSifraPredmeta(String sifraPredmeta) {
 		this.sifraPredmeta = sifraPredmeta;
 	}
-
 
 	public String getNazivPredmeta() {
 		return nazivPredmeta;
 	}
 
-
 	public void setNazivPredmeta(String nazivPredmeta) {
 		this.nazivPredmeta = nazivPredmeta;
 	}
-
 
 	public Integer getSemestar() {
 		return semestar;
 	}
 
-
 	public void setSemestar(Integer semestar) {
 		this.semestar = semestar;
 	}
-
 
 	public Integer getGodinaStudija() {
 		return godinaStudija;
 	}
 
-
 	public void setGodinaStudija(Integer godinaStudija) {
 		this.godinaStudija = godinaStudija;
 	}
 
+	public Profesor getPredmetniProfesor() {
+		return predmetniProfesor;
+	}
+
+	public void setPredmetniProfesor(Profesor predmetniProfesor) {
+		this.predmetniProfesor = predmetniProfesor;
+	}
 
 	public List<Student> getListaStudenata() {
 		return listaStudenata;
 	}
 
-
 	public void setListaStudenata(List<Student> listaStudenata) {
 		this.listaStudenata = listaStudenata;
 	}
-	
-	
-	
+
 }
---
+++ StudentskaSluzba/src/model/Profesor.java
@@ -0,0 +1,11 @@
+package model;
+
+
+/**
+ * Model Predmeta
+ * @author Vaxi RA186/2017
+ *
+ */
+public class Profesor {
+	private String ime;
+}
