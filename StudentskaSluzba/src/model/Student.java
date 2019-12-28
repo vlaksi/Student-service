3154336ab98badc7953c80b596d011408862b23a
Pufke <pufke1998@gmail.com>
2019-12-28 03:32:54 +0100

    Student prototip

--- StudentskaSluzba/src/model/Student.java
+++ StudentskaSluzba/src/model/Student.java
@@ -1,5 +1,84 @@
 package model;
 
+import java.util.Date;
+
 public class Student {
+	private String ime;
+	private String prezime;
+	private Date datumRodjenja;
+	private String adresaStanovanja;
+	private String telefon;
+	private String email;
+	private String brIndexa;
+	private Date datumUpisa;
+	private String godinaStudija;
+	private enum status {
+		B, //Budzet
+		S //Samofinansiranje
+	}
+	
+	private double prosecnaOcena;
+	public String getIme() {
+		return ime;
+	}
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+	public String getPrezime() {
+		return prezime;
+	}
+	public void setPrezime(String prezime) {
+		this.prezime = prezime;
+	}
+	public Date getDatumRodjenja() {
+		return datumRodjenja;
+	}
+	public void setDatumRodjenja(Date datumRodjenja) {
+		this.datumRodjenja = datumRodjenja;
+	}
+	public String getAdresaStanovanja() {
+		return adresaStanovanja;
+	}
+	public void setAdresaStanovanja(String adresaStanovanja) {
+		this.adresaStanovanja = adresaStanovanja;
+	}
+	public String getTelefon() {
+		return telefon;
+	}
+	public void setTelefon(String telefon) {
+		this.telefon = telefon;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getBrIndexa() {
+		return brIndexa;
+	}
+	public void setBrIndexa(String brIndexa) {
+		this.brIndexa = brIndexa;
+	}
+	public Date getDatumUpisa() {
+		return datumUpisa;
+	}
+	public void setDatumUpisa(Date datumUpisa) {
+		this.datumUpisa = datumUpisa;
+	}
+	public String getGodinaStudija() {
+		return godinaStudija;
+	}
+	public void setGodinaStudija(String godinaStudija) {
+		this.godinaStudija = godinaStudija;
+	}
+	public double getProsecnaOcena() {
+		return prosecnaOcena;
+	}
+	public void setProsecnaOcena(double prosecnaOcena) {
+		this.prosecnaOcena = prosecnaOcena;
+	}
+	
+	//TODO Dodati Listu predmeta koje student slusa kad Vladislav otkuca Klasu predmet
 
 }
