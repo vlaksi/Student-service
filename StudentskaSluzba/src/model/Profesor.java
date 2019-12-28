aab03ffd5f2309ba63603166ebdf82d25e87a434
vlaksi <daksrapsody@gmail.com>
2019-12-28 04:35:49 +0100

    #profesor - prototip klase profesor

--- StudentskaSluzba/src/model/Profesor.java
+++ StudentskaSluzba/src/model/Profesor.java
@@ -1,11 +1,130 @@
 package model;
 
+import java.util.Date;
+import java.util.List;
 
 /**
  * Model Predmeta
+ * 
  * @author Vaxi RA186/2017
  *
  */
 public class Profesor {
 	private String ime;
+	private String prezime;
+	private Date datumRodjenja;
+	private String adresaStanovanja;
+	private String telefon;
+	private String email;
+	private String adresaKancelarije;
+	private long brojLicneKarte;
+	private String titula;
+	private String zvanje;
+	private List<Predmet> spisakPredmeta; // spisak predmeta na kojima je profesor
+
+	public Profesor(String ime, String prezime, Date datumRodjenja, String adresaStanovanja, String telefon,
+			String email, String adresaKancelarije, long brojLicneKarte, String titula, String zvanje,
+			List<Predmet> spisakPredmeta) {
+		super();
+		this.ime = ime;
+		this.prezime = prezime;
+		this.datumRodjenja = datumRodjenja;
+		this.adresaStanovanja = adresaStanovanja;
+		this.telefon = telefon;
+		this.email = email;
+		this.adresaKancelarije = adresaKancelarije;
+		this.brojLicneKarte = brojLicneKarte;
+		this.titula = titula;
+		this.zvanje = zvanje;
+		this.spisakPredmeta = spisakPredmeta;
+	}
+
+	public String getIme() {
+		return ime;
+	}
+
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+
+	public String getPrezime() {
+		return prezime;
+	}
+
+	public void setPrezime(String prezime) {
+		this.prezime = prezime;
+	}
+
+	public Date getDatumRodjenja() {
+		return datumRodjenja;
+	}
+
+	public void setDatumRodjenja(Date datumRodjenja) {
+		this.datumRodjenja = datumRodjenja;
+	}
+
+	public String getAdresaStanovanja() {
+		return adresaStanovanja;
+	}
+
+	public void setAdresaStanovanja(String adresaStanovanja) {
+		this.adresaStanovanja = adresaStanovanja;
+	}
+
+	public String getTelefon() {
+		return telefon;
+	}
+
+	public void setTelefon(String telefon) {
+		this.telefon = telefon;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAdresaKancelarije() {
+		return adresaKancelarije;
+	}
+
+	public void setAdresaKancelarije(String adresaKancelarije) {
+		this.adresaKancelarije = adresaKancelarije;
+	}
+
+	public long getBrojLicneKarte() {
+		return brojLicneKarte;
+	}
+
+	public void setBrojLicneKarte(long brojLicneKarte) {
+		this.brojLicneKarte = brojLicneKarte;
+	}
+
+	public String getTitula() {
+		return titula;
+	}
+
+	public void setTitula(String titula) {
+		this.titula = titula;
+	}
+
+	public String getZvanje() {
+		return zvanje;
+	}
+
+	public void setZvanje(String zvanje) {
+		this.zvanje = zvanje;
+	}
+
+	public List<Predmet> getSpisakPredmeta() {
+		return spisakPredmeta;
+	}
+
+	public void setSpisakPredmeta(List<Predmet> spisakPredmeta) {
+		this.spisakPredmeta = spisakPredmeta;
+	}
+
 }
