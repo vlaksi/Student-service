0bad3a94ea0377d74d35305cece5a3da4749b755
vlaksi <daksrapsody@gmail.com>
2019-12-28 04:10:33 +0100

    Prototip klase Predmet

---
+++ StudentskaSluzba/src/model/Predmet.java
@@ -0,0 +1,82 @@
+package model;
+
+import java.util.List;
+
+/**
+ * Model Predmeta
+ * @author Vaxi RA186/2017
+ *
+ */
+public class Predmet {
+	
+	String sifraPredmeta;
+	String nazivPredmeta;
+	Integer semestar;
+	Integer godinaStudija;
+	//TODO Profesor predmetniProfesor;
+	List<Student> listaStudenata;	// spisak studenata koji slusaju predmet
+	
+	
+	public Predmet(String sifraPredmeta, String nazivPredmeta, Integer semestar, Integer godinaStudija,
+			List<Student> listaStudenata) {
+		super();
+		this.sifraPredmeta = sifraPredmeta;
+		this.nazivPredmeta = nazivPredmeta;
+		this.semestar = semestar;
+		this.godinaStudija = godinaStudija;
+		this.listaStudenata = listaStudenata;
+	}
+
+
+	public String getSifraPredmeta() {
+		return sifraPredmeta;
+	}
+
+
+	public void setSifraPredmeta(String sifraPredmeta) {
+		this.sifraPredmeta = sifraPredmeta;
+	}
+
+
+	public String getNazivPredmeta() {
+		return nazivPredmeta;
+	}
+
+
+	public void setNazivPredmeta(String nazivPredmeta) {
+		this.nazivPredmeta = nazivPredmeta;
+	}
+
+
+	public Integer getSemestar() {
+		return semestar;
+	}
+
+
+	public void setSemestar(Integer semestar) {
+		this.semestar = semestar;
+	}
+
+
+	public Integer getGodinaStudija() {
+		return godinaStudija;
+	}
+
+
+	public void setGodinaStudija(Integer godinaStudija) {
+		this.godinaStudija = godinaStudija;
+	}
+
+
+	public List<Student> getListaStudenata() {
+		return listaStudenata;
+	}
+
+
+	public void setListaStudenata(List<Student> listaStudenata) {
+		this.listaStudenata = listaStudenata;
+	}
+	
+	
+	
+}
