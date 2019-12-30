8d55d06ed05b56f1cd9601689b49f955850002bb
Pufke <pufke1998@gmail.com>
2019-12-30 19:16:06 +0100

    #status_bar Implementirana prva verzija status bara

--- StudentskaSluzba/src/view/GlavniProzor.java
+++ StudentskaSluzba/src/view/GlavniProzor.java
@@ -4,9 +4,13 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
-
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /**
  * Klasa koja nam sluzi kao podloga za nas GUI.
@@ -41,16 +45,46 @@ public class GlavniProzor extends JFrame {
 		MojToolbar toolbar = new MojToolbar();
 		add(toolbar, BorderLayout.NORTH);
 		validate(); // osvezavanje dodanog na frejm
-		
+
 		// Postavljena ikonice aplikacije
 		Image img = kit.getImage("img/ikonica.png");
 		setIconImage(img);
-		
+
 		// kreiranje naslednika klase JMenuBar i njeno postavljanje u glavni prozor
 		// aplikacije
 		MojMenyBar meny = new MojMenyBar();
 		this.setJMenuBar(meny);
 		validate(); // osvezavanje dodanog na frejm
+		
+		prikaziStatusBar();
 	}
 
+	
+	
+	
+	private void prikaziStatusBar() {
+
+		// create the status bar panel and shove it down the bottom of the frame
+		JPanel statusPanel = new JPanel();
+		add(statusPanel, BorderLayout.SOUTH);
+		statusPanel.setLayout(new BorderLayout());
+
+		// statusPanel.setPreferredSize(new Dimension(getWidth(), 16));
+		// statusPanel.setLayout(new BoxLayout(statusPanel, BoxLayout.X_AXIS));
+		JLabel statusLabel = new JLabel("Studentska sluzba");
+
+		String timeStamp = new SimpleDateFormat("HH:mm     dd.MM.yyyy").format(Calendar.getInstance().getTime());
+		JLabel vreme = new JLabel(timeStamp);
+		// statusLabel.setHorizontalAlignment(SwingConstants.LEFT);
+
+		// PADING
+		statusLabel.setBorder(BorderFactory.createEmptyBorder(6, 30, 6, 10)); // kako bi napravili border malo,da ne bi
+																				// bilo skroz uz ivice
+		statusPanel.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 30)); // kako bi napravili border malo,da ne bi
+																				// bilo skroz uz ivice
+
+		statusPanel.add(statusLabel, BorderLayout.WEST);
+		statusPanel.add(vreme, BorderLayout.EAST);
+		setVisible(true);
+	}
 }
