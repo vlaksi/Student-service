cc420d084b1faacf76eb7983eaf1021c1be54911
Pufke <pufke1998@gmail.com>
2020-01-02 22:54:40 +0100

    Implementirani listeneri za dodavanje studenata, i prikupljanje podataka iz polja comboBoxa i radio dugmadi

--- StudentskaSluzba/src/view/Dialogs/DodajStudentaDialog.java
+++ StudentskaSluzba/src/view/Dialogs/DodajStudentaDialog.java
@@ -3,6 +3,8 @@ package view.Dialogs;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
@@ -16,7 +18,11 @@ import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
 import view.StudentiJTable;
-
+/**
+ *  Klasa koja predstavlja dialog koji iskace kada pritisnemo dugme za izmenu studenta
+ * @author Pufke
+ *
+ */
 public class DodajStudentaDialog extends JDialog {
 
 	private static final long serialVersionUID = -3924920391540440967L;
@@ -59,7 +65,7 @@ public class DodajStudentaDialog extends JDialog {
 		txtDatumRodjenja.setPreferredSize(dim);
 		panDatumRodj.add(lblDAtumRodj);
 		panDatumRodj.add(txtDatumRodjenja);
-		
+
 		JPanel panAdresaStanovanja = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		JLabel lblAdresaStanovanja = new JLabel("Adresa stanovanja*");
 		lblAdresaStanovanja.setPreferredSize(dim);
@@ -67,8 +73,7 @@ public class DodajStudentaDialog extends JDialog {
 		txtAdresaStanovanja.setPreferredSize(dim);
 		panAdresaStanovanja.add(lblAdresaStanovanja);
 		panAdresaStanovanja.add(txtAdresaStanovanja);
-		
-		
+
 		JPanel panBrojTelefona = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		JLabel lblBrojTelefona = new JLabel("Broj telefona*");
 		lblBrojTelefona.setPreferredSize(dim);
@@ -76,7 +81,7 @@ public class DodajStudentaDialog extends JDialog {
 		txtBrojTelefona.setPreferredSize(dim);
 		panBrojTelefona.add(lblBrojTelefona);
 		panBrojTelefona.add(txtBrojTelefona);
-		
+
 		JPanel panBrojIndexa = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		JLabel lblBrojIndexa = new JLabel("Broj indexa* ");
 		lblBrojIndexa.setPreferredSize(dim);
@@ -84,29 +89,29 @@ public class DodajStudentaDialog extends JDialog {
 		txtBrojIndexa.setPreferredSize(dim);
 		panBrojIndexa.add(lblBrojIndexa);
 		panBrojIndexa.add(txtBrojIndexa);
-		
-		
+
 		JPanel panGodinaStudija = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		JLabel lblGodinaStudija = new JLabel("Trenutna godina studija*");
 		lblGodinaStudija.setPreferredSize(dim);
-		String[] godineStudija = { "I (prva)" , "II (druga)" ,  "III (treca)",  "IV (cetvrta)"};
+		String[] godineStudija = { "I (prva)", "II (druga)", "III (treca)", "IV (cetvrta)" };
 		JComboBox<String> godineComboBox = new JComboBox<String>(godineStudija);
 		godineComboBox.setPreferredSize(dim);
 		panGodinaStudija.add(lblGodinaStudija);
 		panGodinaStudija.add(godineComboBox);
-	
+
 		JPanel panBudzetButton = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		JRadioButton budzetButton = new JRadioButton("Budzet");
 		panBudzetButton.add(budzetButton);
 		budzetButton.setSelected(true);
-		
+
 		JPanel panSamofinansiranjeButton = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		JRadioButton samofinansiranjeButton = new JRadioButton("Samofinansiranje");
 		panSamofinansiranjeButton.add(samofinansiranjeButton);
-		
-		
-		
-	
+
+		//radio batne moramo da dodamo u grupu da bi imali funkcionalnost da je samo jedan radio button selektovan, tj da se medjusobno iskljucuju
+		ButtonGroup group = new ButtonGroup();
+		group.add(budzetButton);
+		group.add(samofinansiranjeButton);
 		
 		panCenter.add(panPrezime);
 		panCenter.add(panIme);
@@ -117,12 +122,10 @@ public class DodajStudentaDialog extends JDialog {
 		panCenter.add(panGodinaStudija);
 		panCenter.add(panBudzetButton);
 		panCenter.add(panSamofinansiranjeButton);
-		
+
 		panCenter.add(Box.createVerticalStrut(25));
 		add(panCenter, BorderLayout.CENTER);
 
-		
-		
 		JPanel panBottom = new JPanel();
 		BoxLayout box = new BoxLayout(panBottom, BoxLayout.X_AXIS);
 		panBottom.setLayout(box);
@@ -140,6 +143,28 @@ public class DodajStudentaDialog extends JDialog {
 
 		add(panBottom, BorderLayout.SOUTH);
 		pack();
+		
+		
+		//Listeneri koji skupljaju text iz polja
+		btnOk.addActionListener(new ActionListener(){
+			   public void actionPerformed(ActionEvent ae){
+			      String imeFieldValue = txtIme.getText();
+			      String prezimeFieldValue = txtPrezime.getText();
+			      String datumRodjenjaFieldValue = txtDatumRodjenja.getText();
+			      String adresaStanovanjaFieldValue = txtAdresaStanovanja.getText();
+			      String brojIndexaFieldValue = txtBrojIndexa.getText();
+			      String godinaStudijaValue = godineComboBox.getSelectedItem().toString();
+			      
+			      budzetButton.setActionCommand(budzetButton.getText());//Ove metode su nam potrebne da bi radio button prikupio informacije
+			      samofinansiranjeButton.setActionCommand(samofinansiranjeButton.getText());
+			      
+			      //Ako je budzet button pritisnut vratice string Budzet ako nije vratice string null
+			      String budzetRadioButtonValue = budzetButton.isSelected()? "Budzet" : "null";
+			      String samofinansiranjeRadioButtonValue = samofinansiranjeButton.isSelected()? "Samofinansiranje" : "null";
+			    
+			
+			   }
+		});
 	}
 
 }
