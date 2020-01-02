3b4be617b20aaadf674a7d773b434d28ebabf76b
vlaksi <daksrapsody@gmail.com>
2020-01-02 21:54:18 +0100

    #sortiranje_predmeta Implementirano sortiranje - ispraviti broj kolona u tabeli

--- StudentskaSluzba/src/view/PredmetiJTable.java
+++ StudentskaSluzba/src/view/PredmetiJTable.java
@@ -6,6 +6,7 @@ import java.awt.Component;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
 import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableRowSorter;
 
 /**
  * Klasa koje reprezentuje konfigurisanje prikaza tabele predmeti.
@@ -17,11 +18,18 @@ public class PredmetiJTable extends JTable {
 
 	private static final long serialVersionUID = -1404313258591137307L;
 	
+	private TableRowSorter<ATMPredmeti> sortiranje;
+	
 	public PredmetiJTable() {
 		this.setRowSelectionAllowed(true);
 		this.setColumnSelectionAllowed(true);
 		this.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		this.setModel(new ATMPredmeti());
+		ATMPredmeti model =new ATMPredmeti();
+		this.setModel(model);
+		
+		sortiranje = new TableRowSorter<ATMPredmeti>(model);
+		sortiranje.setSortable(4, false); // iskljucujem kolonu za spisak studenata
+		this.setRowSorter(sortiranje);
 	}
 	
 	@Override
