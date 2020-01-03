d432bf234712cb9b44232075471505f580cef0ef
Pufke <pufke1998@gmail.com>
2020-01-03 05:53:58 +0100

    #sortiranje_profesora Implementirano sortiranje profesora

--- StudentskaSluzba/src/view/ProfesoriJTable.java
+++ StudentskaSluzba/src/view/ProfesoriJTable.java
@@ -6,22 +6,31 @@ import java.awt.Component;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
 import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableRowSorter;
 
 /**
- * Klasa koja sluzi za reprezentativni nacin prikaza tabele profesora.
+ * Klasa koja sluzi za reprezentativni nacin prikaza tabele profesora i sortiranje profesora.
  * 
- * @author Vaxi
+ * @author Vaxi i Pufke
  *
  */
 public class ProfesoriJTable extends JTable {
 
 	private static final long serialVersionUID = -3442801886235942895L;
 
+	private TableRowSorter<ATMProfesori> sortiranje;
+	
 	public ProfesoriJTable() {
 		this.setRowSelectionAllowed(true);
 		this.setColumnSelectionAllowed(true);
 		this.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		this.setModel(new ATMProfesori());
+		ATMProfesori model = new ATMProfesori();
+		this.setModel(model);
+		
+		sortiranje = new TableRowSorter<ATMProfesori>(model);
+		sortiranje.setSortable(10, false);
+		this.setRowSorter(sortiranje);
 	}
 
 	@Override
