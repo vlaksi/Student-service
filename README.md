d8ea337bca21b5f3098ca77ee32780d0ee2b15cb
vlaksi <daksrapsody@gmail.com>
2020-01-05 18:48:17 +0100

    resen bag: brisanje profesora -> brisanje u predmetu ako je to taj profesor

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 ﻿# Bagovi koje treba resiti:
 
-###### Obicno brisanje profesora treba da aktivira i brisanje profesora na predmetu ako je on na nekom predmetu.
 ###### Obicno brisanje studenta,profesora,predmeta, treba da pozove dijalog koji proveri da li je korisnik siguran da zeli da obrise taj entitet iz baze/tabele.
 ###### Obicno brisanje predmeta treba da aktivira i brisanje tog predmeta sa liste predmeta kod studenta,kao i sa liste predmeta kod profesora, ako student i profesor imaju taj predmet.
 ###### Kada se udje u search i ukuca nesto bezveze, da mu ne dozvoli da klikne na search.
--- StudentskaSluzba/src/controller/ProfesoriController.java
+++ StudentskaSluzba/src/controller/ProfesoriController.java
@@ -1,10 +1,10 @@
 package controller;
+
 import java.util.List;
 
 import model.BazaProfesor;
 import model.Predmet;
 import model.Profesor;
-
 
 /**
  * Klasa koja rukuje reakcijama na dogadjaje entiteta/modela: Profesor.
@@ -14,9 +14,10 @@ import model.Profesor;
  */
 public class ProfesoriController {
 	private static ProfesoriController instance = null;
-	
-	List<Profesor>  listaProfesoraZaSerijalizaciju = SerijalizacijaProfesora.getInstance().deserijalizacijaObjekta();
-	//List<Profesor>  listaProfesoraZaSerijalizaciju = new ArrayList<Profesor>();
+
+	List<Profesor> listaProfesoraZaSerijalizaciju = SerijalizacijaProfesora.getInstance().deserijalizacijaObjekta();
+
+	// List<Profesor> listaProfesoraZaSerijalizaciju = new ArrayList<Profesor>();
 	public static ProfesoriController getInstance() {
 		if (instance == null) {
 			instance = new ProfesoriController();
@@ -26,33 +27,33 @@ public class ProfesoriController {
 
 	private ProfesoriController() {
 	}
-	
+
 //Metoda kojoj prosledjujemo entite profesora
 //i ona je dodaje u bazu podataka, tj u listu 
 	public void dodavanjeProfesora(Profesor profesor) {
-		BazaProfesor.getInstance().dodajProfesora(profesor.getIme(), profesor.getPrezime(),
-				profesor.getDatumRodjenja(), profesor.getAdresaStanovanja(), profesor.getTelefon(), profesor.getEmail(),
-				profesor.getAdresaKancelarije(), profesor.getBrojLicneKarte(), profesor.getTitula(), profesor.getZvanje(),profesor.getPredmeti());
-		//Dodajemo profesora u listu za serijalizaciju
-				listaProfesoraZaSerijalizaciju.add(profesor);
+		BazaProfesor.getInstance().dodajProfesora(profesor.getIme(), profesor.getPrezime(), profesor.getDatumRodjenja(),
+				profesor.getAdresaStanovanja(), profesor.getTelefon(), profesor.getEmail(),
+				profesor.getAdresaKancelarije(), profesor.getBrojLicneKarte(), profesor.getTitula(),
+				profesor.getZvanje(), profesor.getPredmeti());
+		// Dodajemo profesora u listu za serijalizaciju
+		listaProfesoraZaSerijalizaciju.add(profesor);
 	}
 
 	public Profesor getListaProfesora(int selectedRowIndex) {
 		return BazaProfesor.getInstance().getProfesori().get(selectedRowIndex);
 	}
-	
-	public List<Profesor> getListaSvihProfesora(){
+
+	public List<Profesor> getListaSvihProfesora() {
 		return BazaProfesor.getInstance().getProfesori();
 	}
-	
-	
-	public void dodajPredmetProfesoru(Profesor profesor,Predmet predmet) {
-		BazaProfesor.getInstance().dodajPredmetProfesoru(profesor,predmet);
+
+	public void dodajPredmetProfesoru(Profesor profesor, Predmet predmet) {
+		BazaProfesor.getInstance().dodajPredmetProfesoru(profesor, predmet);
 	}
-	
+
 	// metoda koja brise taj predmet iz liste predmeta tog profesora
 	public void izbrisiPredmetProfesora(Profesor profesor, Predmet predmet) {
-		BazaProfesor.getInstance().izbrisiPredmetProfesora(profesor,predmet);
+		BazaProfesor.getInstance().izbrisiPredmetProfesora(profesor, predmet);
 	}
 
 	public List<Profesor> getListaProfesoraZaSerijalizaciju() {
@@ -63,11 +64,21 @@ public class ProfesoriController {
 		this.listaProfesoraZaSerijalizaciju = listaProfesoraZaSerijalizaciju;
 	}
 
-	//Metoda koja brise profesora za proslednje index reda
+	// Metoda koja brise profesora za proslednje index reda
 	public void izbrisiProfesora(int selectedRowIndex) {
-		 BazaProfesor.getInstance().getProfesori().remove(selectedRowIndex);
-		 listaProfesoraZaSerijalizaciju.remove(selectedRowIndex);
+		Profesor profesor = new Profesor();
+		profesor = BazaProfesor.getInstance().getProfesori().get(selectedRowIndex);
+
+		for (Predmet p : PredmetiController.getInstance().getListaSvihPredmeta()) {
+			// prodjem kroz sve predmete i ako imaju tog profesora,uklonimo ga
+			if(p.getPredmetniProfesor().equals(profesor)) {
+				Profesor profa = new Profesor(); profa.setPrezime("nema "); profa.setBrojLicneKarte("profesor");
+				p.setPredmetniProfesor(profa);
+			}
+		}
+		
+		BazaProfesor.getInstance().getProfesori().remove(selectedRowIndex);
+		listaProfesoraZaSerijalizaciju.remove(selectedRowIndex);
 	}
-	
-	
+
 }
