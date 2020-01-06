07ef4cb088ddf8904c96b92e2338ded8eb2e96df
Pufke <pufke1998@gmail.com>
2020-01-06 06:58:00 +0100

    Uradjen help

--- StudentskaSluzba/src/view/Dialogs/HelpDialog.java
+++ StudentskaSluzba/src/view/Dialogs/HelpDialog.java
@@ -37,20 +37,38 @@ public class HelpDialog extends JDialog {
           txtAreaScroll.setViewportView(txtArea);
           txtAreaScroll.setAutoscrolls(true);
 
-		String text = "Стандардан Lorem Ipsum пасус, коришћен јос од 1500. године.\r\n" + 
-				"\"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\"\r\n" + 
+		String text = "KORISCENJE APLIKACIJE:\r\n" + 
+				"-se oslanja na sledece funkcionalnosti:\r\n" + 
+				"1. Dodavanje novog studenta:[u tabu studenti] klikom na prvo dugme[Dodaj] iz toolbara dobijate mogucnost dodavanja entiteta student u sistem.\r\n" + 
+				"2. Izmena postojeceg studenta:[u tabu studenti] klikom na drugo dugme[Izmeni] iz toolbara imate mogucnost menjanja odabranog entiteta student u sistemu.\r\n" + 
+				"3. Brisanje postojeceg studenta:[u tabu studenti] klikom na trece dugme[Izbrisi] iz toolbara imate mogucnost brisanja odabranog entiteta student iz sistema.\r\n" + 
+				"4. Kombinovana pretraga studenta:[u tabu studentii] klikom na dugme za pretragu, mozete vrsiti i prostu i slozenu pretragu. Npr: ime:Marko; ili ime:Marko;prezime:Peric;\r\n" + 
+				"5. Sortiranje studenata: [u tabu studenti] prilikom klika na odgovarajuce ime kolone, vrsi se sortiranje tabele po toj koloni.\r\n" + 
+				"6. Dodavanje studenata na predmet: [u tabu predmeti] klikom na cetvrto dugme[Dodaj studenta] iz toolbara, imate mogucnost dodavanja studenta na taj predmet.\r\n" + 
+				"7. Uklanjanje studenata sa predmeta:[u tabu predmeti] prilikom klika na neko polje iz poslednje kolone, otvara se spisak studenata koji taj predmeta ima, a tu imate mogucnost uklanjanja studenta sa predmeta.\r\n" + 
 				"\r\n" + 
-				"Поглавље 1.10.32 књиге \"de Finibus Bonorum et Malorum\" коју је написао Cicerо 45. године пре нове ере\r\n" + 
-				"\"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?\"\r\n" + 
+				"8. Dodavanje novog predmeta: [u tabu predmeti] klikom na prvo dugme[Dodaj] iz toolbara dobijate mogucnost dodavanja entiteta predmet u sistem.\r\n" + 
+				"9. Izmena postojeceg predmeta: [u tabu predmeti] klikom na drugo dugme[Izmeni] iz toolbara dobijate mogucnost menjanja entiteta predmet u sistemu.\r\n" + 
+				"10.  Brisanje postojeceg predmeta: [u tabu predmeti] klikom na trece dugme[Izbrisi] iz toolbara dobijate mogucnost brisanja entiteta predmet iz sistema.\r\n" + 
+				"11.  Kombinovana pretraga predmeta: [u tabu predmeti] klikom na dugme za pretragu, mozete vrsiti i prostu i slozenu pretragu. Npr: naziv:Analiza.\r\n" + 
+				"12.  Sortiranje predmeta:[u tabu predmeti] prilikom klika na odgovarajuce ime kolone, vrsi se sortiranje tabele po toj koloni.\r\n" + 
 				"\r\n" + 
-				"1914. преведено од стране H. Rackham\r\n" + 
-				"\"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?\"\r\n" + 
+				"13. Dodavanje novog profesora: [u tabu profesori] klikom na prvo dugme[Dodaj] iz toolbara dobijate mogucnost dodavanja entiteta profesor u sistem.\r\n" + 
+				"14. Izmena postojeceg profesora: [u tabu profesori] klikom na drugo dugme[Izmeni] iz toolbara dobijate mogucnost menjanja entiteta profesor u sistemu. \r\n" + 
+				"15. Brisanje postojeceg profesora: [u tabu profesori] klikom na trece dugme[Izbrisi] iz toolbara dobijate mogucnost brisanja entiteta profesor iz sistema. \r\n" + 
+				"16. Kombinovana pretraga profesora: [u tabu profesori] klikom na dugme za pretragu, mozete vrsiti i prostu i slozenu pretragu. Npr: ime:Marko; ili ime:Marko;prezime:Peric;\r\n" + 
+				"17. Sortiranje profesora: [u tabu profesori] prilikom klika na odgovarajuce ime kolone, vrsi se sortiranje tabele po toj koloni.\r\n" + 
 				"\r\n" + 
-				"Поглавље 1.10.33 књиге \"de Finibus Bonorum et Malorum\" коју је написао Cicerо 45. године пре нове ере\r\n" + 
-				"\"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.\"\r\n" + 
+				"18. Dodavanje profesora na predmet: [u tabu predmeti] klikom na peto dugme[Dodaj profesora] iz toolbara, imate mogucnost dodavanja profesora na taj predmet. \r\n" + 
+				"19.  Uklanjanje profesora sa predmeta [u tabu profesori] klikom na poslednju kolonu, za izabrano polje dobijamo spisak predmeta tog profesora i tu imamo mogucnost brisanja profesora sa predmeta.\r\n" + 
 				"\r\n" + 
-				"1914. преведено од стране H. Rackham\r\n" + 
-				"\"On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.\"";
+				"PRECICE:\r\n" + 
+				"CTRL-N: u zavisnosti od taba imate mogucnost dodavanja novog entiteta u sistem.\r\n" + 
+				"CTRL-W: imate mogucnost naglog prekidanja programa bez sacuvavanja izmena u predjasnjem radu.\r\n" + 
+				"CTRL-E: u zavisnosti od taba imate mogucnost menjanja odabranog entiteta.\r\n" + 
+				"CTRL-D: u zavisnosti od taba imate mogucnost brisanja odabranog entiteta iz sistema.\r\n" + 
+				"CTRL-H: dobijanje informacija o precicama i nacinu koriscenja aplikacije.\r\n" + 
+				"CTRL-I: dobijanje informacija o kreatorima i glavnim izvrsiocima programa.";
 
 		txtArea.setText(text);
 
